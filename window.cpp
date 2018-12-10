613e2f480ce53f1fc4c0eb76a5f95c4d85193d84
roman.boursier@gmail.com <roman.boursier@gmail.com>
2018-12-10 14:14:55 +0100

    ajout de l'interactivité

--- window.cpp
+++ window.cpp
@@ -13,42 +13,59 @@ static void init(void);
 static void resize(int w, int h);
 static void draw(void);
 static void quit(void);
-/*!\brief dimensions de la fenêtre */
+/*!\brief dimensions de la fenï¿½tre */
 static int _w = 640, _h = 480;
-/*!\brief identifiant du quadrilatère */
+/*!\brief identifiant du quadrilatï¿½re */
 static GLuint _quad = 0, _sphere = 0;
 /*!\brief identifiants des (futurs) GLSL programs */
 static GLuint _pId = 0;
-/*!\brief identifiant de la texture chargée */
+/*!\brief identifiant de la texture chargï¿½e */
 static GLuint _tId = 0;
 /*!\brief device de capture OpenCV */
-/* essayer d'ouvrir la seconde caméra */
 
-// Si la source est une vidéo :
-  VideoCapture _cap("./assets/discours.mp4");
-// Si la source est une caméra :
-  /* VideoCapture _cap(1); */
+GLfloat color[4] = {0, 1 , 0 , 1};
+int formChoice = 0;
+/* essayer d'ouvrir la seconde camï¿½ra */
+
+// Si la source est une vidï¿½o :
+  //VideoCapture _cap("./assets/discours.mp4");
+// Si la source est une camï¿½ra :
+  VideoCapture _cap(1);
 
 CascadeClassifier * face_cc = NULL;
 CascadeClassifier * eye_cc = NULL;
 
-/*!\brief Création de la fenêtre et paramétrage des fonctions callback.*/
+/*!\brief Change les paramÃ¨tres lors de l'Ã©venement key down.*/
+void interactivity(int keycode){
+    printf("key code %d", keycode );
+    switch (keycode) {
+        case 97:
+            color[0] = (color[0] == 0) ? 1 : 0;
+        break;
+
+        case 122:
+            formChoice = (formChoice == 1) ? 0 : 1;
+         break;
+    }
+}
+
+
+/*!\brief Crï¿½ation de la fenï¿½tre et paramï¿½trage des fonctions callback.*/
 int main(int argc, char ** argv) {
   /* si echec */
 
-// Si la source est une vidéo :
-  if(!_cap.isOpened()){
-    printf("Video is not opened app crashed !! \n");
-    return 1;
-  }
-// Si la source est une caméra :
-/*
+    // Si la source est une vidï¿½o :
+    /*if(!_cap.isOpened()){
+      printf("Video is not opened app crashed !! \n");
+      return 1;
+    }*/
+    // Si la source est une camï¿½ra :
+
   if(!_cap.isOpened()) {
     _cap.open(CV_CAP_ANY);
     if(!_cap.isOpened())
       return 1;
   }
-*/
 
   face_cc = new CascadeClassifier("haarcascade_frontalface_default.xml");
   eye_cc = new CascadeClassifier("haarcascade_eye.xml");
@@ -58,21 +75,24 @@ int main(int argc, char ** argv) {
   /* on modifie les dimensions de capture */
   _cap.set(CV_CAP_PROP_FRAME_WIDTH,  640);
   _cap.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
-  /* on récupère et imprime les dimensions de la vidéo */
+  /* on rï¿½cupï¿½re et imprime les dimensions de la vidï¿½o */
   Size s(_cap.get(CV_CAP_PROP_FRAME_WIDTH), _cap.get(CV_CAP_PROP_FRAME_HEIGHT));
   _w = (int)s.width;
   _h = (int)s.height;
   if(!gl4duwCreateWindow(argc, argv, "GL4Dummies", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
 			 _w, _h, SDL_WINDOW_RESIZABLE | SDL_WINDOW_SHOWN))
     return 1;
+  
   init();
   atexit(quit);
   gl4duwResizeFunc(resize);
   gl4duwDisplayFunc(draw);
   gl4duwMainLoop();
+
   return 0;
 }
-/*!\brief Initialise les paramètres OpenGL.*/
+
+/*!\brief Initialise les paramï¿½tres OpenGL.*/
 static void init(void) {
   _pId = gl4duCreateProgram("<vs>shaders/basic.vs", "<fs>shaders/basic.fs", NULL);
   glClearColor(0.2f, 0.2f, 0.2f, 0.0f);
@@ -92,10 +112,12 @@ static void init(void) {
   GLuint p = 255<<16;
   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 1, 1, 0, GL_BGRA, GL_UNSIGNED_BYTE, &p);
 }
-/*!\brief Paramétrage de la vue (viewPort) OpenGL en fonction
- * des dimensions de la fenêtre.
- * \param w largeur de la fenêtre transmise par GL4Dummies.
- * \param h hauteur de la fenêtre transmise par GL4Dummies.
+
+
+/*!\brief Paramï¿½trage de la vue (viewPort) OpenGL en fonction
+ * des dimensions de la fenï¿½tre.
+ * \param w largeur de la fenï¿½tre transmise par GL4Dummies.
+ * \param h hauteur de la fenï¿½tre transmise par GL4Dummies.
  */
 static void resize(int w, int h) {
   _w  = w; _h = h;
@@ -105,7 +127,9 @@ static void resize(int w, int h) {
   gl4duFrustumf(-0.5, 0.5, -0.5, 0.5, 1, 1000);
   gl4duBindMatrix("modelView");
 }
-/*!\brief Dessin de la géométrie texturée. */
+
+
+/*!\brief Dessin de la gï¿½omï¿½trie texturï¿½e. */
 static void draw(void) {
   static GLfloat dx = 0.0f;
   GLfloat rect[4] = {0, 0, 0, 0};
@@ -115,19 +139,21 @@ static void draw(void) {
   gl4duBindMatrix("modelView");
   gl4duLoadIdentityf();
   Mat frame, gsi;
-  /* récupérer la frame courante */
+  /* rï¿½cupï¿½rer la frame courante */
   _cap >> frame;
   cvtColor(frame, gsi, COLOR_BGR2GRAY);
   vector<Rect> faces;
   face_cc->detectMultiScale(gsi, faces, 1.1, 5);
+
+  gl4duwKeyDownFunc(&interactivity); //Interactivity
+
   for (vector<Rect>::iterator fc = faces.begin(); fc != faces.end(); ++fc) {
     rect[0] = ((*fc).x - _w / 2.0f) / (_w / 2.0f);
     rect[1] = (_h / 2.0f - (*fc).y) / (_h / 2.0f);
     rect[2] = (*fc).width / (GLfloat)_w;
     rect[3] = (*fc).height / (GLfloat)_h;
 
-    GLfloat c[4] = {0, 1, 0, 1};
-    glUniform4fv(glGetUniformLocation(_pId, "color"), 1, c);
+    glUniform4fv(glGetUniformLocation(_pId, "color"), 1, color);
     glUniform1i(glGetUniformLocation(_pId, "test"), 0);
     
     gl4duPushMatrix();
@@ -136,17 +162,14 @@ static void draw(void) {
     //gl4duRotatef(-dx * 1000, 0, 0, 1);
     gl4duSendMatrices();
     gl4duPopMatrix();
-    //décalée réduite
-    gl4dgDraw(_sphere);
+    //dï¿½calï¿½e rï¿½duite
 
-//     /* dessiner le rectangle contenant le visage détecté */
-//     rectangle(frame, (*fc).tl(), (*fc).br(), Scalar(255, 255, 0), 2, CV_AA);
-//     /* détection des yeux */
-//     Mat gsi_roi = gsi(*fc); /* sub image of gsi according to the rest *fc */
-//     vector<Rect> eyes;
-//     eye_cc->detectMultiScale(gsi_roi, eyes, 1.1, 10);
-//     for (vector<Rect>::iterator ec = eyes.begin(); ec != eyes.end(); ++ec)
-//       rectangle(frame, (*fc).tl() + (*ec).tl(), (*fc).tl() + (*ec).br(), Scalar(0, 0, 255), 2, CV_AA);
+    //Change la forme en fonction de la touche enfoncÃ©e
+      if(formChoice == 0) {
+          gl4dgDraw(_quad);
+      } else {
+          gl4dgDraw(_sphere);
+      }
   }
   glBindTexture(GL_TEXTURE_2D, _tId);
   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, frame.cols, frame.rows, 0, GL_BGR, GL_UNSIGNED_BYTE, frame.ptr());
@@ -156,13 +179,17 @@ static void draw(void) {
   gl4duTranslatef(0, 0, -2);
   gl4duSendMatrices();
   glUniform1i(glGetUniformLocation(_pId, "test"), 1);
-  //full centrée
+
+  //full centrï¿½e
   gl4dgDraw(_quad);
   
   dx -= 0.01f;
+
 }
-/*!\brief Appelée au moment de sortir du programme (atexit). Elle
- *  libère les données utilisées par OpenGL et GL4Dummies.*/
+
+
+/*!\brief Appelï¿½e au moment de sortir du programme (atexit). Elle
+ *  libï¿½re les donnï¿½es utilisï¿½es par OpenGL et GL4Dummies.*/
 static void quit(void) {
   if(_tId)
     glDeleteTextures(1, &_tId);
